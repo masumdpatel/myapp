a11a7602b048f5431c6f5c6d576a3be8f85359e8
EC2 Default User <ec2-user@ip-172-31-43-121.ec2.internal>
2023-10-10 11:20:43 +0000

    update install step in plan.sh

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def index():
     return render_template('index.html', tasks=tasks, form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host='0.0.0.0')
 
--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,27 +1,7 @@
-#
-# This file is the heart of your application's habitat.
-# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
-
-# Required.
-# Sets the name of the package. This will be used in along with `pkg_origin`,
-# and `pkg_version` to define the fully-qualified package name, which determines
-# where the package is installed to on disk, how it is referred to in package
-# metadata, and so on.
 pkg_name=myapp
-
-# Required unless overridden by the `HAB_ORIGIN` environment variable.
-# The origin is used to denote a particular upstream of a package.
 pkg_origin=masumdpatel
-
-# Required.
-# Sets the version of the package
 pkg_version="0.1.0"
-
-# Optional.
-# The name and email address of the package maintainer.
 pkg_maintainer="Maintainers <Masum>"
-
-# `origin/package/version/release`.
 pkg_deps=(core/python)
 
 # An array of the package dependencies needed only at build time.
@@ -32,13 +12,12 @@ do_build() {
 }
 
 do_install() {
-  do_default_install
   # copy across the one-file table-setting app plus requirements
   app_dir=$pkg_prefix/$pkg_name
   mkdir $app_dir
   cp app.py $app_dir/
   cp requirements.txt $app_dir/
-  cp templates/ $app_dir/ 
+  cp -r templates/ $app_dir/ 
   #install pip/virtualenv packages on top of python dependency (i.e. site packages)
   pip install --upgrade pip
   pip install virtualenv
