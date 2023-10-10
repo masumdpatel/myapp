f8f9d4bfb32eeeab5fc7ad5c1fe23c09969d977f
EC2 Default User <ec2-user@ip-172-31-43-121.ec2.internal>
2023-10-10 10:50:52 +0000

    changes in plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,3 +1,4 @@
+#
 # This file is the heart of your application's habitat.
 # See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
 
@@ -26,29 +27,16 @@ pkg_deps=(core/python)
 # An array of the package dependencies needed only at build time.
 pkg_build_deps=(core/python)
 
-# script to use $pkg_prefix and then run make to compile the downloaded source.
-# This means the script in the default implementation does
-# ./configure --prefix=$pkg_prefix && make. You should override this behavior
-# if you have additional configuration changes to make or other software to
-# build and install as part of building your package.
 do_build() {
   return 0
 }
 
-# The default implementation is to run make install on the source files and
-# place the compiled binaries or libraries in HAB_CACHE_SRC_PATH/$pkg_dirname,
-# which resolves to a path like /hab/cache/src/packagename-version/. It uses
-# this location because of do_build() using the --prefix option when calling the
-# configure script. You should override this behavior if you need to perform
-# custom installation steps, such as copying files from HAB_CACHE_SRC_PATH to
-# specific directories in your package, or installing pre-built binaries into
-# your package.
 do_install() {
   do_default_install
   # copy across the one-file table-setting app plus requirements
   app_dir=$pkg_prefix/$pkg_name
   mkdir $app_dir
-  cp index.py $app_dir/
+  cp app.py $app_dir/
   cp requirements.txt $app_dir/
   cp templates/ $app_dir/ 
   #install pip/virtualenv packages on top of python dependency (i.e. site packages)
