ecadf06904cf0228441dbbd2d6a265013c4ca3c8
masum <masum.patel@compufytechnolab.com>
2023-10-13 06:05:42 +0000

    changes in plan.ps1

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,33 +1,41 @@
-$pkg_name = "myappwindows"
+$pkg_name = "myapp"
 $pkg_origin = "masumdpatel"
 $pkg_version = "0.1.0"
 $pkg_maintainer = "Maintainers <Masum>"
-$pkg_deps = @("core/python")
+$pkg_deps = "core/python"
 
 # An array of the package dependencies needed only at build time.
-$pkg_build_deps = @("core/python")
+$pkg_build_deps = "core/python"
 
-# Define a function for the build step (you can customize it as needed).
-function Invoke-Build {
+function do_build {
     return 0
 }
 
-# Define a function for the installation step.
-function Invoke-Install {
-    $app_dir = "$env:pkg_prefix\$pkg_name"
-    New-Item -ItemType Directory -Path $app_dir | Out-Null
-    Copy-Item -Path "app.py" -Destination "$app_dir\"
-    Copy-Item -Path "requirements.txt" -Destination "$app_dir\"
-    Copy-Item -Path "templates\" -Destination "$app_dir\" -Recurse
+function do_install {
+    # Copy the one-file table-setting app plus requirements
+    $app_dir = Join-Path -Path $pkg_prefix -ChildPath $pkg_name
+    New-Item -ItemType Directory -Path $app_dir -Force
+    Copy-Item -Path app.py -Destination $app_dir
+    Copy-Item -Path requirements.txt -Destination $app_dir
+    Copy-Item -Path templates -Destination $app_dir -Recurse
 
-    # Install pip/virtualenv packages on top of Python dependency (i.e. site packages)
-    python -m ensurepip --default-pip
+    # Install pip/virtualenv packages on top of python dependency (i.e. site packages)
+    python -m ensurepip
     python -m pip install --upgrade pip
     python -m pip install virtualenv
 
-    # Create virtualenv for our dependencies & install
-    virtualenv "$app_dir\tsenv"
-    & "$app_dir\tsenv\Scripts\Activate"
+    # Create a virtualenv for our dependencies and install them
+    virtualenv $app_dir\tsenv
+    . "$app_dir\tsenv\Scripts\activate"
     python -m pip install -r "$app_dir\requirements.txt"
+
+    # Ensure hab user can activate the virtualenv
+    Get-ChildItem -Path $app_dir -Recurse | ForEach-Object {
+        Set-Acl -Path $_.FullName -AclObject (Get-Acl $_.FullName) -User "hab" -Permission FullControl
+    }
 }
+
+# Call the functions
+do_build
+do_install
 
