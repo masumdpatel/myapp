5c814777b3a55878018d3effa50ffb92a484602d
masumzappsec <masum@zappsec.com>
2023-10-18 16:39:49 +0530

    Fmt the code.

--- install.sh
+++ install.sh
@@ -28,6 +28,10 @@ cd /tmp
 echo "Starting the downloading of package kibana to /tmp" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
 wget https://artifacts.elastic.co/downloads/kibana/kibana-8.10.3-x86_64.rpm
 
+# Install the TSM backup-archive (B/A) client packages
+#echo "Start the Installation of TSM backup-archive (B/A) client packages" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
+#rpm -i TIVsm-BA.x86_64.rpm TIVsm-BAcit.x86_64.rpm >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
+
 echo "Starting the installation of kibana" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
 sudo rpm --install kibana-8.10.3-x86_64.rpm
 
@@ -39,18 +43,27 @@ else
     echo "Installation failed" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
 fi
 
-# Install the TSM backup-archive (B/A) client packages
-echo "Start the Installation of TSM backup-archive (B/A) client packages" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
-rpm -i TIVsm-BA.x86_64.rpm TIVsm-BAcit.x86_64.rpm >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
+echo "Starting the process..."
+service start kibana >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
 
+STATUS=$?
+if [[ $STATUS -eq 0 ]]
+then
+    echo "Service Started" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
+else
+    echo "Service failed to start with error code: $STATUS" >> $LOG_FILES_LOCATION/installation-logs-$DATE.txt
+fi
 # Count the number of Kibana processes running
-kibana_processes=$(ps -ef | grep kibana | wc -l)
+#kibana_processes=$(ps -ef | grep kibana | wc -l)
 
 # Check if there is more than one Kibana process
 
-if [[ $kibana_processes -gt 1 ]]; then
-    echo "Kibana service is already running." >> log.txt
-else
-    systemctl start kibana
-    echo "Kibana has been started." >> log.txt
-fi
+#if [[ $kibana_processes -gt 1 ]]; then
+#    echo "Kibana service is already running." >> log.txt
+#else
+#    systemctl start kibana
+#    echo "Kibana has been started." >> log.txt
+#fi
+
+# Removing the 7 days old log file.
+find $LOG_FILES_LOCATION/ -type f -mtime +7 -exec rm -rf {} \;
--- uninstall.sh
+++ uninstall.sh
@@ -6,14 +6,14 @@ DATE=$(date +"%Y-%m-%d")
 #function for proccess kill
 kill_process() {
         CHECK_PROCESS=$(ps -ef | grep $1 | wc -l)
-        echo "current process running for $1 $CHECK_PROCESS" >> $LOG_FILES_LOCATION/unstallation-logs-$DATE.txt
+        echo "current process running for $1 $CHECK_PROCESS" >> $LOG_FILES_LOCATION/unistallation-logs-$DATE.txt
 
         if [[ $CHECK_PROCESS -gt 1 ]]; then
-        echo "$1 proccess is running" >> $LOG_FILES_LOCATION/unstallation-logs-$DATE.txt
-        echo "killing proccess for $1" >> $LOG_FILES_LOCATION/unstallation-logs-$DATE.txt
+        echo "$1 proccess is running" >> $LOG_FILES_LOCATION/unistallation-logs-$DATE.txt
+        echo "killing proccess for $1" >> $LOG_FILES_LOCATION/unistallation-logs-$DATE.txt
         killall $1
         else
-        echo "$1 proccess is not running" >> $LOG_FILES_LOCATION/unstallation-logs-$DATE.txt
+        echo "$1 proccess is not running" >> $LOG_FILES_LOCATION/unistallation-logs-$DATE.txt
         fi
 }
 
@@ -22,7 +22,7 @@ remove_packages() {
         PACKAGE_LIST=$(rpm -qa | grep $1)
         for package in $PACKAGE_LIST;
         do
-                echo "removing PACKAGE of $1" >> $LOG_FILES_LOCATION/unstallation-logs-$DATE.txt
+                echo "removing PACKAGE of $1" >> $LOG_FILES_LOCATION/unistallation-logs-$DATE.txt
                 rpm -e "$package"
         done
 }
@@ -34,7 +34,7 @@ if [ ! -d "$LOG_FILES_LOCATION" ]; then
 fi
 
 #take tha backup of the dsm.sys and dsm.opt files.
-echo "taking backup of dsm.sys and dsm.opt" >> $LOG_FILES_LOCATION/unstallation-logs-$DATE.txt
+echo "Taking backup of importanat files" >> $LOG_FILES_LOCATION/unistallation-logs-$DATE.txt
 cp -p /home/ec2-user/timestamp.txt /tmp/timestamp-$DATE.txt
 # cp -p /opt/tivoli/tsm/client/ba/bin/dsm.opt /tmp/dsm.opt-$DATE
 
